9da13562dccc30cdafa1b503ca24fbb1b991f6f5
Sanjeev Subnavis <sanjeev.subnavis@techspring.de>
2025-10-27 09:36:32 +0100

    fixed react import

--- src/pages/Cart/components/CartCheckout.js
+++ src/pages/Cart/components/CartCheckout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useCart } from "../../../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import { createOrder, getUser } from "../../../services/DataService";
