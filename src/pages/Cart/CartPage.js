4d11d7c0ceb8d299d2e02f246dc2542482eae820
SanjeevBharadwaj96 <40891008+SanjeevBharadwaj96@users.noreply.github.com>
2025-10-27 09:59:59 +0100

    Update CartPage.js

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,11 +1,11 @@
 import { useTitle } from "../../customHooks/useTitle";
 import { useCart } from "../../context/CartContext";
 import { CartList } from "./components/CartList";
-import { cartEmpty } from "./components/cartEmpty";
+import { CartEmpty } from "./CartEmpty";
 
 export const CartPage = () => {
   useTitle("Cart");
   const { cartList } = useCart();
 
-  return <main>{cartList.length ? <CartList /> : <cartEmpty />}</main>;
+  return <main>{cartList.length ? <CartList /> : <CartEmpty />}</main>;
 };
